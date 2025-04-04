2ead4c46769b867ee3a88c1aa1ebd592824b4be4
Brian Duong <dbiny3css@gmail.com>
2025-04-04 15:25:04 -0500

    Add small component test to see if StyleX styles work

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import * as stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  button: {
+    backgroundColor: "blue",
+    color: "white",
+    padding: "10px 20px",
+    borderRadius: "5px",
+  },
+});
+
+console.log(styles)
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -29,6 +42,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button {...stylex.props(styles.button)}>Click me</button>
     </>
   )
 }
