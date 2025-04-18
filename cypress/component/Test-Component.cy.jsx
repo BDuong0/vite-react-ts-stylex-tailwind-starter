1773793aeeb989f9bad6a8a733d53acd4b05d19d
Brian Duong <dbiny3css@gmail.com>
2025-04-18 06:55:32 -0500

    Add cypress-axe automated testing in Test-Component.cy.jsx

--- cypress/component/Test-Component.cy.jsx
+++ cypress/component/Test-Component.cy.jsx
@@ -1,5 +1,11 @@
-describe('Test-Component.cy.jsx', () => {
-  it('playground', () => {
+describe("Test-Component.cy.jsx", () => {
+  it("playground", () => {
     // cy.mount()
-  })
-})
+  });
+
+  // Additional automatic more comprehensive accessibility test to make sure my components are accessible
+  it("should run accessibility audits", () => {
+    cy.injectAxe();
+    cy.checkA11y();
+  });
+});
