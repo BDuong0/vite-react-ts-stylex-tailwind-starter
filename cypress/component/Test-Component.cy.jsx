deb500740d4b43c0d87767847efb498f56070715
Brian Duong <dbiny3css@gmail.com>
2025-04-19 10:41:26 -0500

    Import component from Test_Component.jsx into Test-Component.cy.jsx

--- cypress/component/Test-Component.cy.jsx
+++ cypress/component/Test-Component.cy.jsx
@@ -1,7 +1,13 @@
+import Test_Component from "../../src/Test_Component";
+
 describe("Test-Component.cy.jsx", () => {
   it("playground", () => {
     // cy.mount()
   });
+
+  cy.mount(<Test_Component />);
+
+  cy.get('[data-cy="test-component"]').as("test-component");
 
   // Additional automatic more comprehensive accessibility test to make sure my components are accessible
   it("should run accessibility audits", () => {
